245341b889b01c7c270787e6eb30182214597893
HarryPostmymail <146686267+HarryPostmymail@users.noreply.github.com>
2023-10-02 04:24:56 +0000

    Variables Lessons

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail="hersh@google.com"
+var accountPassword="12345"
+accountcity = "Jaipur"
+
+
+console.log(accountId)
+
+
+/*
+Prefer not to use Var
+because of issue in block scope and Functional Scope
+*/
